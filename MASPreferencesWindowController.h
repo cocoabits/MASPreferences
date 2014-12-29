b3b1d2f00c15da71ad6fd18e8b7ff83e6e078429
Barijaona Ramaholimihaso <github.com@barijaona.com>
2014-12-29 22:36:45 +0300

    Fix 32 bit compilation problem
    With Xcode 6.1.1 and OS X 10.10.1, we got an error :
    
    MASPreferencesWindowController.m:29:13: Synthesized property 'toolbar' must either be named the same as a compatible instance variable or must explicitly name an instance variable

--- MASPreferencesWindowController.h
+++ MASPreferencesWindowController.h
@@ -23,7 +23,7 @@ __attribute__((__visibility__("default")))
     NSMutableDictionary *_minimumViewRects;
     NSString *_title;
     NSViewController <MASPreferencesViewController> *_selectedViewController;
-	IBOutlet NSToolbar *toolbar;
+	IBOutlet NSToolbar *_toolbar;
 }
 
 @property (nonatomic, readonly) NSMutableArray *viewControllers;
