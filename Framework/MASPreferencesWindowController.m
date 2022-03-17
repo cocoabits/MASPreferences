cocoabits/MASPreferences

commit 135869ca622391cae302987ccbc6d53bd9cf82a4

Various updates for modern macOS. (#61)
* Use resource bundle to support static linkage

* Update minimum target platform to OS X 10.10

* Use preference style for window toolbar in 11.0+

Framework/MASPreferencesWindowController.m
@@ -39,7 +39,7 @@ static NSString * PreferencesKeyForViewBounds (NSString *identifier)
 - (instancetype)initWithViewControllers:(NSArray *)viewControllers title:(NSString *)title
 {
 	NSParameterAssert(viewControllers.count > 0);
-    NSString *nibPath = [[NSBundle bundleForClass:MASPreferencesWindowController.class] pathForResource:@"MASPreferencesWindow" ofType:@"nib"];
+    NSString *nibPath = [[MASPreferencesWindowController resourceBundle] pathForResource:@"MASPreferencesWindow" ofType:@"nib"];
     if ((self = [super initWithWindowNibPath:nibPath owner:self]))
     {
 		_viewControllers = [viewControllers mutableCopy];
@@ -92,6 +92,12 @@ static NSString * PreferencesKeyForViewBounds (NSString *identifier)
         [self.window layoutIfNeeded];
         [self.window setFrameTopLeftPoint:NSPointFromString(origin)];
     }
+
+#ifdef MAC_OS_VERSION_11_0
+    if (@available(macOS 11.0, *)) {
+        [self.window setToolbarStyle:NSWindowToolbarStylePreference];
+    }
+#endif
 
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidMove:)   name:NSWindowDidMoveNotification object:self.window];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidResize:) name:NSWindowDidResizeNotification object:self.window];
@@ -343,6 +349,14 @@ static NSString * PreferencesKeyForViewBounds (NSString *identifier)
     while ([_viewControllers objectAtIndex:selectedIndex] == [NSNull null]);
 
     [self selectControllerAtIndex:selectedIndex];
+}
+
+#pragma mark -
+#pragma mark Helper Functions
+
++ (NSBundle *)resourceBundle {
+    NSBundle *moduleBundle = [NSBundle bundleForClass:MASPreferencesWindowController.class];
+    return [NSBundle bundleWithURL:[NSURL fileURLWithPath:[moduleBundle pathForResource:@"MASPreferences" ofType:@"bundle"]]];
 }
 
 @end

MASPreferences.podspec
@@ -1,5 +1,5 @@
 Pod::Spec.new do |s|
-  s.platform              = :osx
+  s.platform              = :osx, '10.10'
   s.name                  = "MASPreferences"
   s.version               = "1.3"
   s.summary               = "Modern implementation of the Preferences window for OS X apps, used in TextMate, GitBox and Mou."
@@ -8,7 +8,9 @@ Pod::Spec.new do |s|
   s.author                = { "Vadim Shpakovski" => "vadim@shpakovski.com" }
   s.source                = { :git => 'https://github.com/shpakovski/MASPreferences.git', :tag => '1.3' }
   s.source_files          = 'Framework/*.{h,m}'
-  s.resources             = 'Framework/en.lproj/*.xib'
+  s.resource_bundles      = {
+    'MASPreferences' => ['Framework/en.lproj/*.xib']
+  }
   s.exclude_files         = 'README.md', 'LICENSE.md', 'MASPreferences.podspec'
   s.requires_arc          = true
 end

Framework/MASPreferencesWindowController.h
@@ -21,11 +21,7 @@ extern NSString * const kMASPreferencesWindowControllerDidChangeViewNotification
  * Window controller for managing Preference view controllers.
  */
 __attribute__((__visibility__("default")))
-#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
 @interface MASPreferencesWindowController : NSWindowController <NSToolbarDelegate, NSWindowDelegate>
-#else
-@interface MASPreferencesWindowController : NSWindowController
-#endif
 {
 @private
     NSMutableArray *_viewControllers;

commit e4835769577c54205e2885ed08f1d5f098aaa11f

Fix a resource bundle resolving issue introduced in #61.

Framework/MASPreferencesWindowController.m
@@ -359,7 +359,17 @@ static NSString * PreferencesKeyForViewBounds (NSString *identifier)
     return SWIFTPM_MODULE_BUNDLE;
 #else
     NSBundle *moduleBundle = [NSBundle bundleForClass:MASPreferencesWindowController.class];
-    return [NSBundle bundleWithURL:[NSURL fileURLWithPath:[moduleBundle pathForResource:@"MASPreferences" ofType:@"bundle"]]];
+
+    // Lookup for MASPreferences.bundle, which usually comes with CocoaPods's `:linkage => :static`.
+    NSString *resourceBundlePath = [moduleBundle pathForResource:@"MASPreferences" ofType:@"bundle"];
+    if ([resourceBundlePath length]) {
+        NSBundle *resourceBundle = [NSBundle bundleWithURL:[NSURL fileURLWithPath:resourceBundlePath]];
+        if (resourceBundle) {
+            return resourceBundle;
+        }
+    }
+
+    return moduleBundle;
 #endif
 }
 

MASPreferences.podspec
@@ -1,12 +1,12 @@
 Pod::Spec.new do |s|
   s.platform              = :osx, '10.10'
   s.name                  = "MASPreferences"
-  s.version               = "1.4"
+  s.version               = "1.4.1"
   s.summary               = "Modern implementation of the Preferences window for OS X apps, used in TextMate, GitBox and Mou."
   s.homepage              = "https://github.com/shpakovski/MASPreferences"
   s.license               = { :type => 'BSD', :file => 'LICENSE.md' }
   s.author                = { "Vadim Shpakovski" => "vadim@shpakovski.com" }
-  s.source                = { :git => 'https://github.com/shpakovski/MASPreferences.git', :tag => '1.4' }
+  s.source                = { :git => 'https://github.com/shpakovski/MASPreferences.git', :tag => '1.4.1' }
   s.source_files          = 'Framework/*.{h,m}'
   s.resource_bundles      = {
     'MASPreferences' => ['Framework/en.lproj/*.xib']